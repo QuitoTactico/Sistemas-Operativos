2c8cf37681b83056e8abc0a277b592974bd161a7
Esteban <teteban0917@gmail.com>
2024-04-05 18:14:16 -0500

    mejor documentación del .nekoReader

--- Neko/nekoReader.cpp
+++ Neko/nekoReader.cpp
@@ -18,16 +18,23 @@ int main() {
     ifstream file("foto.neko", ios::binary);
     if (file.is_open()) {
         file.read(reinterpret_cast<char*>(&id), 4);
+
         file.read(reinterpret_cast<char*>(&edad), 1);
-        file.read(reinterpret_cast<char*>(&nombreLen), 1);
-        nombre.resize(nombreLen);
-        file.read(&nombre[0], nombreLen);
+
+        file.read(reinterpret_cast<char*>(&nombreLen), 1); //obtiene el tamaño del string (no es necesariamente el largo,
+                                                           // hay caracteres que ocupan más de un byte)
+        nombre.resize(nombreLen);           //le cambia el tamaño al string para que pueda guardar lo que se lee
+        file.read(&nombre[0], nombreLen);   //le pasa la dirección de memoria del primer elemento del string
+                                            // ya la función se encarga de llenar todas las demás direcciones de memoria
+
         file.read(reinterpret_cast<char*>(&abreviacionLen), 1);
         abreviacion.resize(abreviacionLen);
         file.read(&abreviacion[0], abreviacionLen);
+
         file.read(reinterpret_cast<char*>(&descripcionLen), 2);
         descripcion.resize(descripcionLen);
         file.read(&descripcion[0], descripcionLen);
+
         file.close();
 
         cout << "ID: " << id << endl;  //los uint16_t son tratados por cout como enteros, como acá
