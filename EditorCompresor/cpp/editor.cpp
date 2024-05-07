12702e00dc6f929c8c34b73278a86db2ade69fde
MiguelCock <macockc@eafit.edu.co>
2024-05-07 15:45:16 -0500

    finished

--- EditorCompresor/cpp/editor.cpp
+++ EditorCompresor/cpp/editor.cpp
@@ -5,6 +5,7 @@ void editar_buffer(WINDOW* win, std::string& buffer) {
     int ch;
     int row = 0;
     int col = 0;
+    int actual = 0;
     bool running = true;
 
     while (running) {
@@ -15,32 +16,50 @@ void editar_buffer(WINDOW* win, std::string& buffer) {
         ch = wgetch(win);
         switch (ch) {
             case 24:  // Ctrl-X para salir
+                {
+                    char archivo[80];
+                    echo();
+                    mvwprintw(win, row + 1, 0, "Nombre del archivo a guardar: ");
+                    wrefresh(win);
+                    wgetstr(win, archivo);
+                    noecho();
+                    guardar_comprimido(comprimir(buffer), archivo);
+                }
+                
                 running = false;
                 break;
+                
+            /*
             case 19:  // Ctrl-S para guardar
                 {
                     char archivo[80];
                     echo();
-                    mvwgetstr(win, 1, 0, archivo);
+                    mvwprintw(win, 1, 0, "Nombre del archivo a guardar: ");
+                    wrefresh(win);
+                    wgetstr(win, archivo);
                     noecho();
                     guardar_comprimido(comprimir(buffer), archivo);
                 }
                 break;
+            */
             case KEY_BACKSPACE:
             case 127:
-                if (col > 0) {
-                    buffer.erase(col - 1, 1);
+                if (actual > 0) {
+                    buffer.erase(actual - 1, 1);
                     col--;
+                    actual--;
                 }
                 break;
             case '\n':
-                buffer.insert(col, 1, '\n');
+                buffer.insert(actual, 1, '\n');
                 col = 0;
                 row++;
+                actual++;
                 break;
             default:
-                buffer.insert(buffer.begin() + col, ch);
+                buffer.insert(buffer.begin() + actual, ch);
                 col++;
+                actual++;
                 break;
         }
     }
